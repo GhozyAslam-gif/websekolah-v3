bedd6f398cc2f2eb9eb649451de2f7d737de9f51
Ghozy Aslam <masghozyaslam@gmail.com>
2026-06-13 20:56:57 +0700

    Tambahkan File

--- siswa/models.py
+++ siswa/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Siswa(models.Model):
+    # Di bawah ini adalah contoh kolom/field tabelnya, silakan sesuaikan dengan kebutuhanmu
+    nama = models.CharField(max_length=100)
+    kelas = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'siswa'  # Ini wajib supaya nama tabel di database murni bernama "siswa"
